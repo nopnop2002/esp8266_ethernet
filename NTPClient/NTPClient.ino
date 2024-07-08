190b2b861f1c563dba056c5d5cefbe154d0f2ba5
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-07-09 08:54:50 +0900

    added NTPClient

---
+++ NTPClient/NTPClient.ino
@@ -0,0 +1,125 @@
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <W5500lwIP.h>
+//#include <W5100lwIP.h>
+//#include <ENC28J60lwIP.h>
+
+#define CSPIN 16
+
+Wiznet5500lwIP eth(CSPIN);
+//Wiznet5100lwIP eth(CSPIN);
+//ENC28J60lwIP eth(CSPIN);
+byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP ntpUDP;
+
+// Define NTP Client to get time
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+//Week Days
+String weekDays[7]={"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+//Month names
+String months[12]={"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print(ESP.getFullVersion());
+  Serial.println();
+  
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setFrequency(40000000);
+
+  eth.setDefault(); // use ethernet for default route
+  int present = eth.begin(mac);
+  if (!present) {
+    Serial.println("no ethernet hardware present");
+    while(1);
+  }
+  
+  Serial.print("connecting ethernet");
+  while (!eth.connected()) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println();
+  Serial.print("ethernet ip address: ");
+  Serial.println(eth.localIP());
+  Serial.print("ethernet subnetMask: ");
+  Serial.println(eth.subnetMask());
+  Serial.print("ethernet gateway: ");
+  Serial.println(eth.gatewayIP());
+
+
+  Serial.println("Starting NTP Cient");
+  // Initialize a NTPClient to get time
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(0);
+}
+
+void loop() {
+  timeClient.update();
+
+  time_t epochTime = timeClient.getEpochTime();
+  Serial.print("Epoch Time: ");
+  Serial.println(epochTime);
+  
+  String formattedTime = timeClient.getFormattedTime();
+  Serial.print("Formatted Time: ");
+  Serial.println(formattedTime);  
+
+  int currentHour = timeClient.getHours();
+  Serial.print("Hour: ");
+  Serial.println(currentHour);  
+
+  int currentMinute = timeClient.getMinutes();
+  Serial.print("Minutes: ");
+  Serial.println(currentMinute); 
+   
+  int currentSecond = timeClient.getSeconds();
+  Serial.print("Seconds: ");
+  Serial.println(currentSecond);  
+
+  String weekDay = weekDays[timeClient.getDay()];
+  Serial.print("Week Day: ");
+  Serial.println(weekDay);    
+
+  //Get a time structure
+  struct tm *ptm = gmtime ((time_t *)&epochTime); 
+
+  int monthDay = ptm->tm_mday;
+  Serial.print("Month day: ");
+  Serial.println(monthDay);
+
+  int currentMonth = ptm->tm_mon+1;
+  Serial.print("Month: ");
+  Serial.println(currentMonth);
+
+  String currentMonthName = months[currentMonth-1];
+  Serial.print("Month name: ");
+  Serial.println(currentMonthName);
+
+  int currentYear = ptm->tm_year+1900;
+  Serial.print("Year: ");
+  Serial.println(currentYear);
+
+  //Print complete date:
+  String currentDate = String(currentYear) + "-" + String(currentMonth) + "-" + String(monthDay);
+  Serial.print("Current date: ");
+  Serial.println(currentDate);
+
+  Serial.println("");
+
+  delay(2000);
+}
---
+++ NTPClient/README.md
@@ -0,0 +1,26 @@
+# NTPClient   
+Get time from a NTP server and keep it in sync.   
+This project uses NTPClient.
+https://www.arduino.cc/reference/en/libraries/ntpclient/
+
+
+# Screen Shot   
+```
+ethernet ip address: 192.168.10.113
+ethernet subnetMask: 255.255.255.0
+ethernet gateway: 192.168.10.1
+Starting NTP Cient
+sendNTPPacket
+Update from NTP Server
+Epoch Time: 1720482357
+Formatted Time: 23:45:57
+Hour: 23
+Minutes: 45
+Seconds: 57
+Week Day: Monday
+Month day: 8
+Month: 7
+Month name: July
+Year: 2024
+Current date: 2024-7-8
+```
